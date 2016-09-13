84b5c64483bcdffd0551da459e07e2a2e31eb2ec
Joseph Martinsen <joseph@martinsen.com>
2016-09-13 01:36:44 -0500

    HW03

--- HW02/HW02/Source.cpp
+++ HW02/HW02/Source.cpp
@@ -8,52 +8,54 @@ using namespace std;
 #include <string>
 #include <cmath>
 
-int main()
-{
-	float a,b,c, dr, x0, x1;
+int main() {
+	float a, b, c, dr, x0, x1;
 
 	// Gather info from user
 	cout << "Enter a, b, and c on a single line seperated by a space" << endl;
-
 	cin >> a;
-
 	cin >> b;
-
 	cin >> c;
 
-	// Calculate the Discriminate
-	dr = pow(b, 2) - 4 * a*c;
 
-	/*
-		Determine what type of solution
-		Positive = 2 real roots
-		0 = There is one real root
-		Negative = There are two complex roots
-	*/
-	if (dr == 0)
-	{
-		// One real root
-		x0 = (-b + sqrt(dr)) / (2 * a);
-		cout << "x = " << x0;
-	}
-	else if (dr > 0)
-	{
-		// 2 real roots
-		x0 = (-b - sqrt(dr)) / (2 * a);
-		x1 = (-b + sqrt(dr)) / (2 * a);
-		cout << "x = " << x0 << endl;
-		cout << "x = " << x1 << endl;
-	}
-	else if (dr < 0)
-	{
-		// 2 complex roots
-		float i0;
-		x0 = -b / (2 * a);
-		i0 = sqrt(-dr);
+	// Determine if a = 0, if so solve linear equation
+	if(a == 0 && b !=0 && c != 0) {
+        cout <<  b << "x + " << c << " = 0" << endl;
+        x0 = -c/b;
+        cout << x0 << endl;
+	} else if(b == 0) {
+        cout << "Unable to determine root(s)." << endl;
+	} else {
+        cout << a << "x^2 + " << b << "x + " << c << " = 0" << endl;
+        // Calculate the Discriminate
+        dr = pow(b, 2) - 4 * a*c;
+
+        /*
+            Determine what type of solution
+            Positive = 2 real roots
+            0 = There is one real root
+            Negative = There are two complex roots
+        */
+        if (dr == 0) {
+            // One real root
+            x0 = (-b + sqrt(dr)) / (2 * a);
+            cout << "x = " << x0;
+        } else if (dr > 0) {
+            // 2 real roots
+            x0 = (-b - sqrt(dr)) / (2 * a);
+            x1 = (-b + sqrt(dr)) / (2 * a);
+            cout << "x = " << x0 << endl;
+            cout << "x = " << x1 << endl;
+        } else if (dr < 0) {
+            // 2 complex roots
+            float i0;
+            x0 = -b / (2 * a);
+            i0 = sqrt(-dr)/( 2* a);
 
 
-		cout << "x = " << x0 << " - " << i0 << "i" << endl;
-		cout << "x = " << x0 << " + " << i0 << "i" << endl;
+            cout << "x = " << x0 << " - " << i0 << "i" << endl;
+            cout << "x = " << x0 << " + " << i0 << "i" << endl;
+        }
 	}
 
 	return 0;
---
+++ HW03/hw03.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int codeLength;
+    int digit;
+    int i = 0;
+
+    srand(time(0));
+    // Ask user for how digits in code
+    cout << "Enter number of digits in code (3, 4 or 5): ";
+    cin >> codeLength;
+    vector<int> codeVector;
+
+
+    // if = 0, ask user for code, else, generate random code
+    if(codeLength == 0) {
+        // Get code from user
+        cout << "Enter code: ";
+        cin >> digit;
+        int numberDigits = 0;
+
+        // Convert int into Array
+        while (digit) {
+            codeVector.push_back(digit % 10);
+            digit /= 10;
+            ++numberDigits;
+        }
+
+        int trueDigits;
+        cout << "Enter number of digits in code: ";
+        cin >> trueDigits;
+
+        // Output inputed number to guess
+        cout << "Number to guess: ";
+        for(int i = 0; i <= numberDigits-trueDigits+1;++i) {
+            cout << 0;
+        }
+        for(i = numberDigits-1; i>=0;--i){
+            cout << codeVector.at(i);
+        }
+    } else {
+        // Generate random code of codeLength
+        for(int i = 0;i < codeLength;++i) {
+            codeVector.push_back(rand()%10);
+            for(int j = 0; j < i; ++j) {
+                while(codeVector.at(i) == codeVector.at(j)) {
+                    codeVector.at(i) = (rand()%10);
+                    j=0;
+                }
+            }
+        }
+        cout << "Number to guess: ";
+        for(int i = 0; i<codeLength;++i){
+            cout << codeVector.at(i);
+        }
+    }
+
+      return 0;
+    }
+
