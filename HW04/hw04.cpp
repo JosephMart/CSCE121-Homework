47cf56b9f2ce777d225df175c360306aaf8ae673
Joseph Martinsen <joseph@martinsen.com>
2016-09-16 23:17:54 -0500

    HW3 First Submission

--- HW04/hw04.cpp
+++ HW04/hw04.cpp
@@ -10,6 +10,7 @@ int main() {
     int codeLength;
     int digit;
     int i = 0;
+    int j = 0;
 
     srand(time(0));
     // Ask user for how digits in code
@@ -23,7 +24,7 @@ int main() {
         // Get code from user
         cout << "Enter code: ";
         cin >> digit;
-        int numberDigits = 0;
+        codeLength = 0;
 
         int trueDigits;
         cout << "Enter number of digits in code: ";
@@ -34,15 +35,15 @@ int main() {
             //codeVector.push_back(digit % 10);
             codeVector.insert(codeVector.begin(),digit % 10);
             digit /= 10;
-            ++numberDigits;
+            ++codeLength;
         }
 
         // Add extra zeros
-        for( i = 1; i <= trueDigits-numberDigits;++i) {
-
+        for( i = 1; i <= trueDigits-codeLength;++i) {
             codeVector.insert(codeVector.begin(),0);
         }
 
+        codeLength = trueDigits;
         // Output inputed number to guess
         cout << "Number to guess: ";
 
@@ -53,7 +54,7 @@ int main() {
         // Generate random code of codeLength
         for( i = 0;i < codeLength;++i) {
             codeVector.push_back(rand()%10);
-            for(int j = 0; j < i; ++j) {
+            for( j = 0; j < i; ++j) {
                 while(codeVector.at(i) == codeVector.at(j)) {
                     codeVector.at(i) = (rand()%10);
                     j=0;
@@ -66,6 +67,57 @@ int main() {
             cout << codeVector.at(i);
         }
     }
-      return 0;
+
+    int guess;
+    vector<int> guessVector;
+    bool gameContinue = true;
+
+    // Time to guess
+    while(gameContinue){
+        int bulls = 0;
+        int cows = 0;
+        int guessCount = 0;
+        guessVector.clear();
+
+        cout << endl << "Enter guess: ";
+        cin >> guess;
+
+        // Convert guess into guess Vector
+        while (guess) {
+            //codeVector.push_back(digit % 10);
+            guessVector.insert(guessVector.begin(),guess % 10);
+            guess /= 10;
+            ++guessCount;
+        }
+
+        // Check if guess was supposed to start with a 0
+        if(guessCount < codeVector.size()) {
+            guessVector.insert(guessVector.begin(),0);
+        }
+
+        // Compare guessVector to codeVector
+        for(i = 0;i < guessVector.size(); ++i) {
+            for( j = 0;j < codeVector.size(); ++j) {
+                if(guessVector.at(i) == codeVector.at(j)) {
+                    if(i == j) {
+                        ++bulls;
+                    } else {
+                        ++cows;
+                    }
+                }
+            }
+        }
+
+        cout << bulls << " bulls";
+
+        if (bulls == codeVector.size()) {
+            cout << "  -  Correct!";
+            gameContinue = false;
+        } else {
+            cout << endl;
+            cout << cows << " cows";
+        }
+    }
+    return 0;
     }
 
