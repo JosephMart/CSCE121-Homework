c116722e75d38c9a3ea8ee3ff00d1c5b82b78800
Joseph Martinsen <joseph@martinsen.com>
2016-09-21 22:46:05 -0500

    HW04 Error Handeling Additions

--- HW01/HW01/Source.cpp
+++ HW01/HW01/Source.cpp
@@ -1,47 +1,57 @@
-// Homework 1
-// Written by Joseph Martinsen
-// August 29th, 2016
-// CSCE 121-504
-// Dr. John Moore
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <ctime>
 
 using namespace std;
-#include <iostream>
-#include <string>
 
 int main()
 {
-	// Initialize Variables
-	string firstName, lastName;
-	float birth, feet, inches, avgGrowth, userHeight, age;
+	srand(time(NULL));
 
-	// Gather required information from the user
-	cout << "What is your first name: ";
-	cin >> firstName;
+	vector<int> nums{ 583, 249, 129, 938, 256, 765 };
 
-	cout << "What is your last name: ";
-	cin >> lastName;
+	/*
+	// When you uncomment this block, comment out the vector declaration/definition/initialization above
+	vector<int> nums;
+	// Hint: this is very similar to how the censor words were read in
+	//       in the unnecessary censorship example.
+	// Step 1: declare/define/initialize/open an input stream for file "nums.dat"
 
-	cout << "What is your birth year: ";
-	cin >> birth;
+	// Step 2: check if file opened successfully
 
-	cout << "What is your height?\nFeet: ";
-	cin >> feet;
+	// Step 3: read data
+	// if opened, then read values from file into vector nums
+	// hint: you can read data inside a while loop with condition infile >> a where a is an int type
 
-	cout << "Inches: ";
-	cin >> inches;
+	// Step 5: close (Do nothing will automatically close when stream variable is destroyed.)
+	*/
 
-	// Calculate info
-	userHeight = ((feet * 12) + inches)*2.54;
-	age = 2016 - birth;
-	avgGrowth = userHeight / age;
-	
-	// Output info to user
-	cout << "Hello " << firstName << " " << lastName << ". You were " << age
-		<< " years old in 2016, and your height is " << userHeight <<
-		" cm. \nThat means you grew an average of " << avgGrowth << " cm per year. (Assuming that you were 51 cm at brith)";
+	// Output values in vector
+	for (unsigned int i = 0; i < nums.size(); ++i) {
+		cout << setw(5) << nums.at(i);
+		if ((i + 1) % 5 == 0) cout << endl; // new line for each 5 numbers
+	}
+	cout << endl << endl;
 
-	// Need to get rid of
-	system("PAUSE");
+	sort(nums.begin(), nums.end());
 
-	return 0;
+	// Steps 1 & 2: declare/define/initialize/open an output stream for file "stats.dat"
+	// Step 3: check if opened successfully
+
+	double median = (nums.size() % 2) ? nums.at((nums.size() - 1) / 2) : (nums.at((nums.size() - 1) / 2) + nums.at((nums.size()) / 2)) / 2.0;
+	// above probably needs a comment, but as a challenge figure out what's going on...
+
+	// Step 4: write to stream (convert below to output to file stream instead of cout)
+	cout << "Min: " << nums.at(0) << endl;
+	cout << "Max: " << nums.at(nums.size() - 1) << endl;
+	cout << "Median " << median << endl;
+
+	// Step 5: close (Do nothing will automatically close when stream variable is destroyed.)
+
+	//system("pause");            // Uncomment for Visual Studio
 }
--- HW04/hw04.cpp
+++ HW04/hw04.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <cmath>
 #include <stdlib.h>
 #include <ctime>
+#include <limits>
 using namespace std;
 
 int main() {
+    bool gameContinue = true;
+    bool again;
     int codeLength;
     int digit;
     int i = 0;
     int j = 0;
-
+    int guess;
+    vector<int> guessVector;
+    vector<int> codeVector;
     srand(time(0));
     // Ask user for how digits in code
     cout << "Enter number of digits in code (3, 4 or 5): ";
     cin >> codeLength;
-    vector<int> codeVector;
+    // Check if input is acceptable
+    again = (codeLength == 0 || codeLength == 3 || codeLength == 4 ||
+      codeLength == 5) ? false:true;
 
+    while (cin.fail() || again) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(),'\n');
+      cout << "Enter number of digits in code (3, 4 or 5): ";
+      cin >> codeLength;
+      again = (codeLength == 0 || codeLength == 3 || codeLength == 4 ||
+        codeLength == 5) ? false:true;
+    }
     // if = 0, ask user for code, else, generate random code
     if(codeLength == 0) {
+        int trueDigits;
+        codeLength = 0;
         // Get code from user
         cout << "Enter code: ";
         cin >> digit;
-        codeLength = 0;
-
-        int trueDigits;
         cout << "Enter number of digits in code: ";
         cin >> trueDigits;
 
@@ -35,18 +47,15 @@ int main() {
             digit /= 10;
             ++codeLength;
         }
-
         // Add extra zeros if need be
-        for( i = 1; i <= trueDigits-codeLength;++i) {
+        for(i = 1; i <= trueDigits-codeLength;++i) {
             codeVector.insert(codeVector.begin(),0);
         }
-
         // For later use in guesses
         codeLength = trueDigits;
 
         // Output inputed number to guess
         cout << "Number to guess: ";
-
         for(i = 0; i<=codeVector.size()-1;++i){
             cout << codeVector.at(i);
         }
@@ -68,32 +77,30 @@ int main() {
         }
     }
     // Continue into guessing portion
-    int guess;
-    vector<int> guessVector;
-    bool gameContinue = true;
-
     while(gameContinue){
         int bulls = 0;
         int cows = 0;
         int guessCount = 0;
         guessVector.clear();
-
         cout << endl << "Enter guess: ";
         cin >> guess;
-
+        // Check if it is acceptable input
+        while (cin.fail()) {
+          cin.clear();
+          cin.ignore(numeric_limits<streamsize>::max(),'\n');
+          cout << endl << "Enter guess: ";
+          cin >> guess;
+        }
         // Convert guess into guess Vector
         while (guess) {
-            //codeVector.push_back(digit % 10);
             guessVector.insert(guessVector.begin(),guess % 10);
             guess /= 10;
             ++guessCount;
         }
-
         // Check if guess was supposed to start with a 0
         if(guessCount < codeVector.size()) {
             guessVector.insert(guessVector.begin(),0);
         }
-
         // Compare guessVector to codeVector
         for(i = 0;i < guessVector.size(); ++i) {
             for( j = 0;j < codeVector.size(); ++j) {
@@ -106,9 +113,7 @@ int main() {
                 }
             }
         }
-
         cout << bulls << " bulls";
-
         if (bulls == codeVector.size()) {
             cout << "  -  Correct!";
             gameContinue = false;
@@ -119,4 +124,3 @@ int main() {
     }
     return 0;
     }
-
