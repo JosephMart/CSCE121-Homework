25ccaf60d7ff9910924d5b74a533f5e27a2a9bc3
Joseph Martinsen <joseph@martinsen.com>
2016-11-14 01:12:26 -0600

    HW12 Init

---
+++ HW12/Node.h
@@ -0,0 +1,28 @@
+#ifndef NODE
+#define NODE
+
+// Include the header file for the data struct/class if you use one.
+// For example, if you have a Data class in Data.h, put
+//  #include "Data.h"
+// below.
+
+struct Node {
+	// Add your member variable for the data fields here.
+	// You can use an extra struct/class for storing the data. In that case, put
+	// your definition of the data class in a separate header file, for example:
+	// data.h
+
+	// Pointer to the next node in the linked list
+	Node* next;
+
+	// Default constructor
+	Node() : next(nullptr) {
+		// Initialize your data members properly inside the function body
+	}
+
+	Node(/*FIXME */) : next(nullptr) {
+		// Initialize your data members properly inside the function body
+	}
+};
+
+#endif
