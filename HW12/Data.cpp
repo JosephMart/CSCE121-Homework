2bb3d8ab4c5e36b1ec18731a5e9ac3638e82f107
Joseph Martinsen <joseph@martinsen.com>
2016-11-18 23:09:06 -0600

    46/60 HOLLLLLYUYYYY CRAPPPPP

---
+++ HW12/Data.cpp
@@ -0,0 +1,12 @@
+// #include <ostream>
+// #include <iostream>
+// #include "Data.h"
+// using namespace std;
+
+// ostream& operator<<(std::ostream& os, const Data1& v) {
+//     os << "Location: " << v.getLocation() << endl;
+//     os << "Year: " << v.getYear() << endl;
+//     os << "Month: " << v.getMonth() << endl;
+//     os << "Temperature: " << n. << endl;
+//     return os;
+// }
--- HW12/Node.h
+++ HW12/Node.h
@@ -1,6 +1,8 @@
 #ifndef NODE
 #define NODE
+#include <iostream>
 #include "Data.h"
+using namespace std;
 
 // Include the header file for the data struct/class if you use one.
 // For example, if you have a Data class in Data.h, put
@@ -12,27 +14,30 @@ struct Node {
 	// You can use an extra struct/class for storing the data. In that case, put
 	// your definition of the data class in a separate header file, for example:
 	// data.h
-	Data1 data;
+	// Data1 data;
+	int Location;
+    int Year;
+    int Month;
+    double tempValue;
 
 	// Pointer to the next node in the linked list
 	Node* next;
 
 	// Default constructor
-	Node() : next(nullptr) {
-		// Initialize your data members properly inside the function body
-		data.setLocation(0);
-		data.setYear(0);
-		data.setMonth(0);
-		data.setValue(0);
-	}
+	Node() : Location(0), Year(0), Month(0), tempValue(0), next(nullptr) {}
 
-	Node(Data dataEntry) : next(nullptr) {
-		// Initialize your data members properly inside the function body
-		data.setLocation(dataEntry.getLocation());
-		data.setYear(dataEntry.getYear());
-		data.setMonth(dataEntry.getMonth());
-		data.setValue(dataEntry.getValue());
+	Node(int Location, int Year, int Month, double tempValue) : Location(Location), Year(Year), Month(Month), tempValue(tempValue), next(nullptr) {}
+		
+	friend std::ostream& operator<<(std::ostream& os, const Node& n) {
+		os << "Location: " << n.Location << endl;
+		os << "Year: " << n.Year << endl;
+		os << "Month: " << n.Month << endl;
+		os << "Temperature: " << n.tempValue << endl;
+		return os;
 	}
+	// The function below is written. Do not modify it
+	virtual ~Node() {}
 };
+
 
 #endif
