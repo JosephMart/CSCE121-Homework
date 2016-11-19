cc187a13811e3c78897780120e43ca4d8fae9965
Joseph Martinsen <joseph@martinsen.com>
2016-11-18 18:42:43 -0600

    Mostly load database

--- HW12/TemperatureDB.cpp
+++ HW12/TemperatureDB.cpp
@@ -10,14 +10,27 @@ void TemperatureDatabase::loadData(const string& filename) {
 	ifstream myfile (filename);
 	int i = 0;
 	if (myfile.is_open()) {
-    	while ( getline (myfile,line) ) {
+    	while ( myfile >> line ) {
 			int location;
 			int year;
 			int month;
 			double temperature;
-
+			
+			if(i == 0) {
+				location = atoi(line.c_str());
+				++i;
+			} else if (i == 1) {
+				year =  atoi(line.c_str());
+				++i;
+			} else if (i == 2) {
+				month ==  atoi(line.c_str());
+				++i;
+			} else if (i == 3) {
+				temperature =  atoi(line.c_str());
+				i = 0;
+			} // else throw error
+			
       		records.insert(location, year, month, temperature);// << line << '\n';
-			++i;
 			cout << i << endl;
     	}
     	myfile.close();
