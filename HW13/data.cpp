c9c00f47ad4a155fdc7a3e78a9305f3ab5357983
Joseph Martinsen <joseph@martinsen.com>
2016-11-25 17:00:57 -0600

    Combine Query into Data
    Succesful build on vocareum

--- HW13/data.cpp
+++ HW13/data.cpp
@@ -29,3 +29,29 @@ ostream& operator<<(ostream& os, const Data& d) {
 	os << d.location << ' ' << d.year << ' ' << d.month << ' ' << d.temperature;
 	return os;
 }
+
+// Query struct
+
+Query::Query() {}
+
+Query::Query(int location, string avgMode, int year0, int year1)
+	: location(location), avgMode(avgMode), year0(year0), year1(year1), tempAvg(1773)
+{}
+
+istream& operator>>(istream& is, Query& q) {
+	is >> q.location >> q.avgMode >> q.year0 >> q.year1;
+	return is;
+}
+
+ostream& operator<<(ostream& os, const Query& q) {
+	os << q.location << ' ' << q.year0 << ' ' << q.year1 << ' ' << q.avgMode << ' ';
+
+	if (q.tempAvg == 1773 || q.tempAvg != q.tempAvg) {
+		os << "unknown";
+	} else {
+		os << q.tempAvg;
+	}
+
+
+	return os;
+}
--- HW13/data.h
+++ HW13/data.h
@@ -2,6 +2,7 @@
 #define DATA_H
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct Data {
@@ -18,5 +19,21 @@ struct Data {
 
 istream& operator>>(istream& is, Data& d);
 ostream& operator<<(ostream& os, const Data& d);
+
+struct Query {
+	Query();
+	Query(int location, string avgMode, int year0, int year1);
+
+	// bool operator<(const Query& d) const;
+
+	int location;
+	string avgMode;
+	int year0;
+	int year1;
+	double tempAvg;
+};
+
+istream& operator>>(istream& is, Query& q);
+ostream& operator<<(ostream& os, const Query& q);
 
 #endif
